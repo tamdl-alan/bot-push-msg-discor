8ef00796a2499f2b3d219dfac4b25a030d4c62c1
Tam Dang Linh <linhtam.tamdl@gmail.com>
2025-07-01 11:18:42 +0700

    Fix message schedule to send at 16:00 on weekdays

--- remaind.js
+++ remaind.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
   console.log(`Bot is online as ${client.user.tag}`);
 
   // Lên lịch gửi tin nhắn lúc 16h mỗi ngày
-  schedule.scheduleJob('* * * * *', () => {
+  schedule.scheduleJob('0 16 * * 1-5', () => {
     const channel = client.channels.cache.get(channelId);
     if (channel) {
       channel.send(
