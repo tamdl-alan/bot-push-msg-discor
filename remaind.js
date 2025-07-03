83e6e22b60c68475cf908be91406117e2027382c
Tam Dang Linh <linhtam.tamdl@gmail.com>
2025-07-03 16:27:40 +0700

    Refactor message content to remove deployment rule and adjust numbering for clarity

--- remaind.js
+++ remaind.js
@@ -18,11 +18,10 @@ client.on('ready', () => {
         `⏰ Đã đến lúc kiểm tra thông tin ${new Date().getDate() + '/' + new Date().getMonth() + '/' + new Date().getFullYear()}!\n` +
         `Hãy nhớ kiểm tra các công việc của bạn và hoàn thành chúng nhé! 💪\n\n` +
         `1> 📌 Daily report trên sheet internal [Daily report](https://docs.google.com/spreadsheets/d/1sv49utfAbiMVuqflAItapMmMpJSuj1h2T7weGug1Dxs/edit?gid=1476772392#gid=1476772392) \n` +
-        `2> 📌 Rule deployment [Deployment history](https://docs.google.com/spreadsheets/d/1GOvnOKYSAis738mhg0qEfEymRWbsWQ5Z67qgRkf6-TE/edit?gid=1979966378#gid=1979966378)\n` +
-        `3> 📌 Nếu có release thì check lại [Release Note](https://docs.google.com/spreadsheets/d/1G7GVl7yBEqmTk0zOQfockFsnH8TxNBzrKexArsHGrJY/edit?gid=313226932#gid=313226932)\n` +
-        `4> 📌 Kiểm tra log time trên [Jira](https://r1repo.atlassian.net/plugins/servlet/ac/com.tda.timesheet.report/main?project.key=IPLATBILL&project.id=10013)\n` +
-        `5> 📌 Link [Jinken](http://192.168.0.232:8080/)\n` +
-        `6> 📌 Khi xong task thì nhớ update status trên [Redmine](http://35.73.146.111:4000/projects/cis/issues)\n\n\n`
+        `2> 📌 Nếu có release thì check lại [Release Note](https://docs.google.com/spreadsheets/d/1G7GVl7yBEqmTk0zOQfockFsnH8TxNBzrKexArsHGrJY/edit?gid=313226932#gid=313226932)\n` +
+        `3> 📌 Kiểm tra log time trên [Jira](https://r1repo.atlassian.net/plugins/servlet/ac/com.tda.timesheet.report/main?project.key=IPLATBILL&project.id=10013)\n` +
+        `4> 📌 Link [Jinken](http://192.168.0.232:8080/)\n` +
+        `5> 📌 Khi xong task thì nhớ update status trên [Redmine](http://35.73.146.111:4000/projects/cis/issues)\n\n\n`
       );
       console.log(`✅ Tin nhắn đã được gửi thành công [${new Date()}]!`);
     } else {
